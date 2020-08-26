adcddd31fabb9a1fc8902e8134f5b20cacce5d56
renqiukai <renqiukai@qq.com>
2020-08-26 17:06:13 +0800

    增加员工列表接口和批量离职员工接口

--- cdApi/user.py
+++ cdApi/user.py
@@ -5,102 +5,38 @@
 @版本    :1.0
 '''
 
+from typing import List
 from .base import base
 
 
-class auth(base):
+class user(base):
     def __init__(self, token):
         super().__init__(token)
 
-    def list(self, pageNum=1, pageSize=10):
-        api_name = "manager/sysuser/list"
+    def list(self,
+             storeIdKey=None, companyId=None, mobile=None,
+             pageNum=1, pageSize=10,):
+        api_name = "manager/user/listemp"
         data = {
             "pageNum": pageNum,
             "pageSize": pageSize,
+            "storeIdKey": storeIdKey,
+            "companyId": companyId,
+            "mobile": mobile,
         }
         return self.request(api_name, data)
 
-    def getAllData(self, pageNum=1, pageSize=10):
-        while 1:
-            response = self.list(pageNum, pageSize)
-            rows = response.get("data")
-            rows = rows.get("dataList")
-            if len(rows) >= pageSize:
-                pageNum += 1
-                yield rows
-            else:
-                yield rows
-                return
+    def batch_update(self, data):
+        api_name = "manager/user/batch_update"
+        return self.request(api_name, data)
 
-    def create(self, name, specCode):
-        api_name = ""
+    def batch_delete(self, idList: List, status=0):
+        """
+        批量离职
+        """
+        api_name = "manager/user/batch_update"
         data = {
-            "id": 10270
+            "idList": idList,
+            "status": status,
         }
         return self.request(api_name, data, method="POST")
-
-    def read(self, _id):
-        api_name = "manager/store/info"
-        data = {
-            "id": _id,
-        }
-        response = self.request(api_name, data, method="GET")
-        # print(response)
-        return response
-
-    def update(self, data):
-        api_name = "manager/store/update"
-        response = self.request(api_name, data, method="POST")
-        # print(response)
-        return self.response(response)
-
-    def updateDemo(self):
-        data = {
-            'logoUrl': "",
-            'storeCode': "",
-            'storeType': 0,
-            'address': "上海市浦东新区浦东南路2250号",
-            'province': "上海市",
-            'city': "上海市",
-            'area': "浦东新区",
-            'longitude': "123.51",
-            'latitude': "31.20",
-            'level': 2,
-            'managerMobile': "",
-            'managerName': "",
-            'name': "测试店（滨江）",
-            'parentId': 10269,
-            'type': 0,
-            'takeStatus': 0,
-            'accountId': None,
-            'regionValue': ["上海市", "上海市", "浦东新区"],
-        }
-        _id = 10270
-        self.update(_id, data)
-
-    def updateGPS(self, _id, lat, lng):
-        data = self.read(_id)
-        data = data.get("data")
-        data["longitude"] = lng
-        data["latitude"] = lat
-        self.update(data)
-
-    def delete(self):
-        pass
-
-    def updateUserAuth(self, data):
-        api_name = "manager/sysuser/update"
-        response = self.request(api_name, data, method="POST")
-        print(response)
-        return self.response(response)
-
-    def updateUserAuthDemo(self, companyId):
-        data = {
-            "name": "秋锴1",
-            "mobile": "13801587423",
-            "roleIds": [36], "status2": True, "storeType": 1,
-            "companyId": companyId, "companyId2": [],
-            "storeId": "", "storeId2": [], "id": 13160, "storeIds": [companyId],
-            "status": 1, "type": None, "corpId": None, "isAdmin": 0
-        }
-        (self.updateUserAuth(data))
--- test.py
+++ test.py
@@ -3,11 +3,34 @@
 @Date: 2020-04-26 15:01:20
 @Description: 
 '''
+from logging import log
 from cdApi.coupon_pack import couponPack
 from cdApi.product import product
-from cdApi.store import store
+from cdApi.user import user
+import pandas as pd
+from loguru import logger
 
 if __name__ == "__main__":
-    token = "eee4e7af-74ac-436d-8fb2-87bcd7f305e8"
-    s = store(token=token)
-    s.update_manager_mobile(29561,"13801234567")
+    token = "88b81d1a-c412-4e83-9740-690e8811d6c3"
+    s = user(token=token)
+    filename = "C:\\Users\\renqi\\CloudStation\\我的工作\\桌面\\只有手机号码用户(1).xls"
+    df = pd.read_excel(filename)
+    idList = []
+    for idx, mobile in enumerate(df["手机号码"], start=1):
+        users = s.list(mobile=mobile)
+        users = users.get("data")
+        totalCount = users.get("totalCount")
+        # print(users)
+        if totalCount:
+            users = users.get("dataList")
+            user = users[0]
+            user_id = user.get("id")
+            idList.append(user_id)
+            logger.success({"idx": idx, "mobile": mobile, "user_id": user_id})
+        else:
+            logger.error({"idx": idx, "mobile": mobile})
+        if len(idList) % 50 == 0 and len(idList) > 0:
+            result = s.batch_delete(idList=idList)
+            logger.success({"result": result, "idList": idList})
+            idList = []
+            # break
