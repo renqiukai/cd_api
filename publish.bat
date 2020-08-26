49ade70e3d7d0ac03e8f19ca2956949206e09c88
renqiukai <renqiukai@qq.com>
2020-08-26 17:06:41 +0800

    1.1.3 澧炲姞鍛樺伐鍒楄〃鎺ュ彛鍜屾壒閲忕鑱屽憳宸ユ帴鍙?

--- publish.bat
+++ publish.bat
@@ -3,13 +3,8 @@ rd dist /Q /S
 rd cd_api.egg-info /Q /S
 
 git add -A
-<<<<<<< HEAD
-git commit -m "1.1.1"
-git push git master
-=======
-git commit -m "1.1.3 合并代码"
+git commit -m "1.1.3 增加员工列表接口和批量离职员工接口"
 git push github master
->>>>>>> e023a89b9b595ab581ba24e52bc07dd918020606
 
 python setup.py sdist build
 twine upload dist/*
